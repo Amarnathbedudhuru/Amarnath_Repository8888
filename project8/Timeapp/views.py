f08e92cbe46c250e46f104da6f1a7beceedf09a1
Amarnathbedudhuru <155337565+Amarnathbedudhuru@users.noreply.github.com>
2024-01-01 16:36:41 +0530

    Update views.py

--- project8/Timeapp/views.py
+++ project8/Timeapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 import datetime
@@ -7,8 +9,8 @@ def display_time(request):
 
 	server_time=datetime.datetime.now()
 
-	dict={'time':server_time}
+	return HttpResponse(server_time)
 
 
 
-	return render(request=request, template_name='Timeapp/time.html',context=dict)
+	
